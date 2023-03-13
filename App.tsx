46798b13907253ce34a726809797dfd7746fc486
Pratik-Hulawale <83464018+Pratik-Hulawale@users.noreply.github.com>
2023-03-13 12:45:20 +0530

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
+import Contacts from './Components/Contacts';
 import './style.css';
 
 export default function App() {
+  const cde = [
+    {
+      id: 1,
+      Name: 'a',
+    },
+    {
+      id: 2,
+      Name: 'b',
+    },
+  ];
   return (
     <div>
-  
+      <Contacts Contact={cde} />
     </div>
   );
 }
---
+++ Components/Contacts.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+function Contacts(props) {
+  console.log(props.Contact[0].id);
+  return <h1>From Contact</h1>;
+}
+export default Contacts;
